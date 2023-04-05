123604531fc5d15b490b80d01e0ec0bb93563f92
LeeTaekJoo <11ltj12@naver.com>
2023-04-05 12:04:11 +0900

    first commit

---
+++ src/main/java/com/shinhan/controller/FirstServlet.java
@@ -0,0 +1,51 @@
+package com.shinhan.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @WebServlet : 요청URL에 의해 실행할 Servlet연결 (Servlet Mapping)
+ * 주의점 : 요청URL에 반드시 /로 시작한다.
+ *서블릿 매핑에서 /를 생략하면 유효하지 않은 <url-pattern>
+ */
+@WebServlet({ "/FirstServlet", "/first", "/ff" })
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+    public FirstServlet() {
+    	System.out.println("FirstServlet의 생성자이다.");
+    }
+
+	
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("FirstServlet init");
+	}
+
+	
+	public void destroy() {
+		System.out.println("FirstServlet destroy");
+	}
+
+	// service는 모든 요청방식을 처리하고자 하는 경우 사용
+//	public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		System.out.println("FirstServlet service");
+//	}
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("FirstServlet doGet");
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	
+		System.out.println("FirstServlet doPost");
+		
+	}
+
+}
